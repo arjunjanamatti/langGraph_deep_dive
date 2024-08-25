1d9df78029582074d7356e20cf8d39c818a7af20
Arjun Janamatti <arjun.janamatti@rockwellautomation.com>
2024-08-25 12:23:02 +0530

    adding config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class input_variables:
+    class input_variables:
+        CHROMA_PATH = "chroma"
+        DATA_PATH = "data"
+        DATASET_URL = "jamescalam/ai-arxiv2-semantic-chunks"
+
